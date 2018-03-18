eb8c3bb02336e832079557c64c007500f6687040
IvoDimitrov <ivopdimitrov@gmail.com>
2018-03-18 17:11:20 +0200

    Setting up Browser Desired Capabilities

--- src/test/java/chapter01/GoogleSearchTest.java
+++ src/test/java/chapter01/GoogleSearchTest.java
@@ -17,8 +17,13 @@ public class GoogleSearchTest {
 
     @Before
     public void setUp() {
+        // setting up Browser Desired Capabilities
+        System.setProperty("webdriver.chrome.driver",
+                "C:\\Program Files (x86)\\Google\\Chrome\\chromedriver_win32\\chromedriver.exe");
         // Launch a new Chrome instance
         driver = new ChromeDriver();
+        System.setProperty("webdriver.gecko.driver",
+                "C:\\Program Files (x86)\\Mozilla Firefox\\geckodriver\\geckodriver.exe");
         // Launch a new Firefox instance
         // driver = new FirefoxDriver();
         // Maximize the browser window
@@ -46,7 +51,7 @@ public class GoogleSearchTest {
             }
         });
 
-        assertEquals("Selenium testing tools cookbook - Google Search",
+        assertEquals("Selenium testing tools cookbook - Google Търсене",
                 driver.getTitle());
     }
 
