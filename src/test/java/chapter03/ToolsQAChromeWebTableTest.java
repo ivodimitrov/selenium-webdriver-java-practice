afc0313165cf4c8c76e8b4b8495f71a01a835867
Ivo Dimitrov <ivopdimitrov@gmail.com>
2018-06-08 11:11:01 +0300

    Added ToolsQAChromeWebTableTest.

---
+++ src/test/java/chapter03/ToolsQAChromeWebTableTest.java
@@ -0,0 +1,70 @@
+package chapter03;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+
+import static org.testng.AssertJUnit.assertEquals;
+
+public class ToolsQAChromeWebTableTest {
+
+    private WebDriver driver;
+
+    @Before
+    public void setUp() {
+        // Setting up Browser Desired Capabilities
+        System.setProperty("webdriver.chrome.driver",
+                "./src/test/resources/drivers/chromedriver.exe");
+
+        // Launch a new Chrome instance
+        System.out.println("Starting driver...");
+
+        driver = new ChromeDriver();
+
+        System.out.println("Started driver.");
+
+        // Maximize the browser window
+        driver.manage().window().maximize();
+        // Navigate to Google
+        driver.get("http://toolsqa.com/automation-practice-table/");
+    }
+
+    @After
+    public void tearDown() {
+        // Close the browser
+        driver.quit();
+
+        System.out.println();
+        System.out.println("Driver is quited.");
+    }
+
+    @Test
+    public void testWebTable() {
+
+        WebElement simpleTable = driver.findElement(By.id("content"));
+
+        // Get all rows
+        List<WebElement> rows =
+                simpleTable.findElements(By.tagName("tr"));
+        // Verify number of rows in table
+        assertEquals(6, rows.size());
+
+        // Print data from each row
+        for (WebElement row : rows) {
+            List<WebElement> cols = row.findElements(By.tagName("td"));
+            for (WebElement col : cols) {
+                System.out.print(col.getText() + "\t");
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+        System.out.println("Test passed.");
+    }
+}
