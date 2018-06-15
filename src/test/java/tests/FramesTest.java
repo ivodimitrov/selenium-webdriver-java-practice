f3309bc4f539739624e4a51410f6a8a0039ffd85
ivopdimitrov <ivopdimitrov@gmail.com>
2018-06-15 19:39:39 +0300

    Added FramesTest.

--- src/test/java/tests/FramesTest.java
+++ src/test/java/tests/FramesTest.java
@@ -13,6 +13,7 @@ public class FramesTest extends BaseTest {
     @Test
     public void testFrameWithIdOrName() {
         goToPageAndWaitPageToLoad(IFRAME_PAGE);
+
         try {
             // Activate the frame on left side using it's id attribute
             getWebDriver().switchTo().frame("IF1");
@@ -27,6 +28,35 @@ public class FramesTest extends BaseTest {
         try {
             // Activate the frame on right side using it's name attribute
             getWebDriver().switchTo().frame("IF2");
+            // Get an element from the frame on right side and verify it's contents
+            WebElement tab1Tab = getWebDriver().findElement(By.
+                    cssSelector("#ui-id-1"));
+            assertEquals("Tab 1", tab1Tab.getText());
+        } finally {
+
+            // Activate the Page, this will move context from frame back to the Page
+            getWebDriver().switchTo().defaultContent();
+        }
+    }
+
+    @Test
+    public void testFrameByIndex() {
+        goToPageAndWaitPageToLoad(IFRAME_PAGE);
+
+        try {
+            // Activate the frame in middle using it's index. Index starts at 0
+            getWebDriver().switchTo().frame(0);
+            // Get an element from the frame on left side and verify it's contents
+            WebElement buttonButton = getWebDriver().findElement(By.
+                    cssSelector("#submit"));
+            assertEquals("Button", buttonButton.getText());
+        } finally {
+            // Activate the Page, this will move context from frame back to the Page
+            getWebDriver().switchTo().defaultContent();
+        }
+        try {
+            // Activate the frame on right side using it's name attribute
+            getWebDriver().switchTo().frame(1);
             // Get an element from the frame on right side and verify it's contents
             WebElement tab1Tab = getWebDriver().findElement(By.
                     cssSelector("#ui-id-1"));
