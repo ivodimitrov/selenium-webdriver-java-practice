63928246aa05da181724a1a75cdbafe1f68816dd
ivopdimitrov <ivopdimitrov@gmail.com>
2018-06-15 21:43:14 +0300

    Updated Links. Added SwitchWindowTest.

--- src/main/java/utils/Links.java
+++ src/main/java/utils/Links.java
@@ -10,14 +10,17 @@ public class Links {
     public static final String TABLE_PAGE = "http://toolsqa.com/automation-practice-table/";
     public static final String DRAG_DROP_PAGE = "http://demoqa.com/droppable/";
     public static final String IFRAME_PAGE = "http://toolsqa.com/iframe-practice-page/";
+    public static final String SWITCH_WINDOWS_PAGE = "http://toolsqa.com/automation-practice-switch-windows/";
     public static final String GMAIL_PAGE = "https://www.google.com/gmail/about/";
     public static final String GOOGLE_PAGE = "http://www.google.com/ncr";
+
     private static final String HOME_PAGE_TITLE = "demo form";
     private static final String ALERTS_PAGE_TITLE = "toolsqa";
     private static final String SELECTABLE_PAGE_TITLE = "selectable";
     private static final String TABLE_PAGE_TITLE = "demo table";
     private static final String DRAG_DROP_PAGE_TITLE = "droppable";
     private static final String IFRAME_PAGE_TITLE = "toolsqa";
+    private static final String SWITCH_WINDOWS_PAGE_TITLE = "a new title";
     private static final String GMAIL_PAGE_TITLE = "gmail";
     private static final String GOOGLE_PAGE_TITLE = "google";
 
@@ -31,6 +34,7 @@ public class Links {
         pagesMap.put(TABLE_PAGE, TABLE_PAGE_TITLE);
         pagesMap.put(DRAG_DROP_PAGE, DRAG_DROP_PAGE_TITLE);
         pagesMap.put(IFRAME_PAGE, IFRAME_PAGE_TITLE);
+        pagesMap.put(SWITCH_WINDOWS_PAGE, SWITCH_WINDOWS_PAGE_TITLE);
         pagesMap.put(GMAIL_PAGE, GMAIL_PAGE_TITLE);
         pagesMap.put(GOOGLE_PAGE, GOOGLE_PAGE_TITLE);
 
---
+++ src/test/java/tests/SwitchWindowsTest.java
@@ -0,0 +1,43 @@
+package tests;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import utils.BaseTest;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static utils.Links.SWITCH_WINDOWS_PAGE;
+
+public class SwitchWindowsTest extends BaseTest {
+
+    @Test
+    public void testWindowUsingName() {
+        // Go to page
+        goToPageAndWaitPageToLoad(SWITCH_WINDOWS_PAGE);
+
+        // Store WindowHandle of parent browser window
+        String parentWindowId = getWebDriver().getWindowHandle();
+        // Clicking New Message Window button will open new child window
+        getWebDriver().findElement(By.
+                cssSelector("#content > p:nth-child(5) > button:nth-child(1)")).click();
+        try {
+            // Switch to the new Message window using name
+            getWebDriver().switchTo().window("MsgWindow");
+            try {
+                // Check the driver context is in Message window
+                assertEquals("Message window does not exist", "", getWebDriver().getTitle());
+                assertTrue("Message window does not exist", getWebDriver().getPageSource().
+                        contains("Knowledge increases by sharing but not by saving."));
+            } finally {
+                // Close the Message window
+                getWebDriver().close();
+            }
+        } finally {
+            // Switch to the parent browser window
+            getWebDriver().switchTo().window(parentWindowId);
+        }
+        // Check driver context is in parent browser window
+        assertEquals("A new title is here",
+                getWebDriver().getTitle());
+    }
+}
