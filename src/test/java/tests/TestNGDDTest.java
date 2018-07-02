f5b184c80da3c7b53b5c0a3c09bad5caaaff687f
Ivo Dimitrov <ivopdimitrov@gmail.com>
2018-07-02 12:01:02 +0300

    Updated TestNGDDTest.

--- src/test/java/tests/TestNGDDTest.java
+++ src/test/java/tests/TestNGDDTest.java
@@ -31,20 +31,16 @@ public class TestNGDDTest extends BaseTest {
 
     @BeforeTest
     public void setUp() {
-        // Create a new instance of the Firefox driver
         // driver = new FirefoxDriver();
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        // driver.get("http://cookbook.seleniumacademy.com/bmicalculator.html");
         driver.get(BMICALCULATOR_PAGE);
     }
 
     @Test(dataProvider = "testData")
     public void testBMICalculator(String height, String
             weight, String bmi, String category) {
-
-        // goToPageAndWaitPageToLoad(BMICALCULATOR_PAGE);
 
         WebElement heightField = driver.findElement(By.
                 name("heightCMS"));
@@ -71,7 +67,6 @@ public class TestNGDDTest extends BaseTest {
 
     @AfterTest
     public void tearDown() {
-        driver.close();
         driver.quit();
     }
 }
