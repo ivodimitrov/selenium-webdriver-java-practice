ea0d96dce6488e18212ec90b43fe289adf23536f
Ivo Dimitrov <ivopdimitrov@gmail.com>
2018-07-10 12:56:45 +0300

    Refactor WebTableTest & ToolsQaTablePage. Added ToolsQaTableBaseTest.

--- src/main/java/pages/TablePage.java
+++
@@ -1,6 +0,0 @@
-package pages;
-
-public class TablePage {
-
-    //TODO
-}
---
+++ src/main/java/pages/ToolsQaTablePage.java
@@ -0,0 +1,33 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+import java.util.List;
+
+public class ToolsQaTablePage extends Page {
+    @FindBy(id = "content")
+    private WebElement simpleTable;
+
+    public ToolsQaTablePage(WebDriver webDriver) {
+        super(webDriver);
+    }
+
+    // Get all rows
+    public List<WebElement> getAllRows() {
+        return simpleTable.findElements(By.tagName("tr"));
+    }
+
+    // Print data from each row
+    public void printAllRows() {
+        for (WebElement row : getAllRows()) {
+            List<WebElement> cols = row.findElements(By.tagName("td"));
+            for (WebElement col : cols) {
+                System.out.print(col.getText() + "\t");
+            }
+            System.out.println();
+        }
+    }
+}
